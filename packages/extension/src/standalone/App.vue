40b3677e768fa1ac0711d0fa70bad636f5964aa0
Ido Shamun <ido@elegantmonkeys.com>
2018-12-26 19:17:04 +0200

    feat(extension): random header call-to-action button

--- packages/extension/src/standalone/App.vue
+++ packages/extension/src/standalone/App.vue
@@ -1,14 +1,44 @@
 <template>
-  <div>
-    <DaHeader></DaHeader>
+  <div class="app">
+    <da-header></da-header>
+    <div class="content">
+      <div class="content__header">
+        <h4>/# News for developers #/</h4>
+        <a class="header__cta shadow " :href="cta.link" target="_blank"
+           @mouseup="ctaClick" :style="cta.style">
+          <span class="header__cta__text">// {{cta.text}}</span>
+          <img class="header__cta__image" :src="`/logos/${cta.logo}.svg`" v-if="cta.logo"/>
+          <svgicon class="header__cta__image" :icon="cta.icon" v-else/>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import DaHeader from '../components/DaHeader.vue';
+import ctas from './ctas';
 
 export default {
   components: { DaHeader },
+
+  data() {
+    return {
+      cta: ctas[Math.floor(Math.random() * ctas.length)],
+    };
+  },
+
+  methods: {
+    ctaClick() {
+      // ga('send', 'event', this.cta.name, 'Click');
+    },
+  },
+
+  mounted() {
+    if (this.cta.icon) {
+      import(`@daily/components/icons/${this.cta.icon}`);
+    }
+  }
 };
 </script>
 
@@ -23,15 +53,62 @@ body {
   margin: 0;
 }
 
+a {
+  text-decoration: none;
+}
+
 .separator {
   display: block;
   border-left: 1px dotted var(--theme-separator);
   height: 32px;
   width: 1px;
 }
-</style>
-<style scoped>
-div {
+
+.app {
   color: var(--theme-primary);
+
+  & .header {
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  margin: 76px 42px 76px 76px;
+}
+
+.content__header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  & h4 {
+    text-transform: uppercase;
+    color: var(--theme-secondary);
+  }
+}
+
+.header__cta {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: auto;
+  border-radius: 8px;
+}
+
+.header__cta__text {
+  margin: 8px 8px 8px 16px;
+
+  @mixin micro2;
+}
+
+.header__cta__image {
+  width: 20px;
+  height: 20px;
+  margin: 8px;
+  color: var(--color-salt-10);
 }
 </style>
---
+++ packages/extension/src/standalone/ctas.js
@@ -0,0 +1,57 @@
+const ctas = [{
+    text: 'Leave your feedback',
+    icon: 'github',
+    link: 'https://github.com/elegantmonkeys/daily',
+    name: 'Github',
+    style: {
+        color: 'white',
+        background: 'black',
+    },
+}, {
+    text: 'Join Daily community',
+    icon: 'medium',
+    link: 'https://medium.com/daily-now',
+    name: 'Medium',
+    style: {
+        color: 'white',
+        background: '#008bff',
+    },
+}, {
+    text: 'Contribute on our Slack',
+    logo: 'slack',
+    link: 'https://slack.dailynow.co',
+    name: 'Slack',
+    style: {
+        color: '#838886',
+        background: 'white',
+    },
+}, {
+    text: 'Follow real-time updates',
+    icon: 'twitter',
+    link: 'https://twitter.com/dailynowco',
+    name: 'Twitter',
+    style: {
+        color: 'white',
+        background: '#00a2f9',
+    },
+}, {
+    text: 'We need your review :)',
+    logo: 'chrome_store',
+    link: 'https://chrome.google.com/webstore/detail/daily-discover-web-techno/jlmpjdjjbgclbocgajdjefcidcncaied/reviews',
+    name: 'Chrome',
+    style: {
+        color: '#838886',
+        background: 'white',
+    },
+}, {
+    text: 'We need your review :)',
+    logo: 'firefox',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/daily/reviews/',
+    name: 'Firefox',
+    style: {
+        color: 'white',
+        background: '#0e1028',
+    },
+}];
+
+export default ctas;
