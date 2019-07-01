f17e06165fbdefd90aa26936502ed1b2a2c5cd1e
Ido Shamun <ido@elegantmonkeys.com>
2019-07-01 13:21:36 +0300

    feat(storybook): add DaContext storybook

---
+++ packages/components/stories/DaContext.stories.js
@@ -0,0 +1,28 @@
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+
+import DaContext from '../src/components/DaContext.vue';
+
+storiesOf('DaContext', module)
+  .add('default', () => ({
+    components: { DaContext },
+    template: `
+    <div>
+      <button class="btn-icon" title="Open context menu" @click="openMenu">
+        <svgicon icon="menu"/>
+      </button>
+      <DaContext ref="context">
+        <button class="btn btn-menu" @click="broken">Broken link</button>
+        <button class="btn btn-menu" @click="nsfw">Report NSFW</button>
+      </DaContext>
+    </div>
+    `,
+    methods: {
+      openMenu(event) {
+        this.$refs.context.open(event);
+      },
+      broken: action('broken'),
+      nsfw: action('nsfw'),
+    },
+  }));
+
