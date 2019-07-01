daa7982d5acec17373c8f34d862bf2d80558d7e4
Ido Shamun <ido@elegantmonkeys.com>
2019-07-01 14:47:53 +0300

    feat(storybook): add icon stories

---
+++ packages/components/stories/icon.stories.js
@@ -0,0 +1,19 @@
+import { storiesOf } from '@storybook/vue';
+import { withKnobs, select } from '@storybook/addon-knobs';
+
+const requireIcons = require.context('../icons', false, /.js$/);
+const icons = requireIcons.keys()
+  .filter(r => r !== './index.js')
+  .map(r => r.match(/\.\/(\w+)\.js/)[1]);
+
+storiesOf('icon', module)
+  .addDecorator(withKnobs)
+  .add('default', () => ({
+    template: '<svgicon :icon="icon" :key="icon"/>',
+    props: {
+      icon: {
+        default: select('icon', icons, icons[0]),
+      },
+    },
+  }));
+
