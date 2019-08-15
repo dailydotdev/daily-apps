3a706bc21182992e1dbc7f022257e45067956939
Ido Shamun <ido@dailynow.co>
2019-08-15 10:55:15 +0300

    fix(components): truncate tags in insane mode (#26)
    Use `DaLineClamp` to limit the tags in insane posts to one line.
    
    Closes #25

--- packages/components/__tests__/DaCardPost.js
+++ packages/components/__tests__/DaCardPost.js
@@ -1,6 +1,15 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
+import svgicon from 'vue-svgicon';
+import DaLineClamp from '../src/components/DaLineClamp.vue';
+import DaCard from '../src/components/DaCard.vue';
 import DaCardPost from '../src/components/DaCardPost.vue';
 import posts from '../src/posts';
+
+const localVue = createLocalVue();
+
+localVue.use(svgicon);
+localVue.component('da-line-clamp', DaLineClamp);
+localVue.component('da-card', DaCard);
 
 it('should emit bookmark event on click', () => {
   const post = posts[0];
@@ -25,7 +34,8 @@ it('should set bookmark button title', () => {
 it('should set bookmark button title when bookmarked', () => {
   const post = posts[1];
   const wrapper = shallowMount(DaCardPost, { propsData: { post } });
-  expect(wrapper.find('.card__footer__bookmark').element.title).toEqual('Remove bookmark');
+  expect(wrapper.find('.card__footer__bookmark').element.title)
+    .toEqual('Remove bookmark');
 });
 
 it('should set bookmarked class when bookmarked', () => {
@@ -34,10 +44,11 @@ it('should set bookmarked class when bookmarked', () => {
   expect(wrapper.element.classList.contains('bookmarked')).toEqual(true);
 });
 
-it('should set tags title', () => {
+it('should set tags', () => {
   const post = posts[1];
-  const wrapper = shallowMount(DaCardPost, { propsData: { post } });
-  expect(wrapper.find('.card__tags').element.title).toEqual('#javascript,#webdev,#html,#html5');
+  const wrapper = mount(DaCardPost, { localVue, propsData: { post } });
+  expect(wrapper.find('.card__tags span').element.innerHTML)
+    .toEqual('#javascript,#webdev,#html,#html5');
 });
 
 it('should show notification', () => {
--- packages/components/__tests__/DaInsanePost.js
+++ packages/components/__tests__/DaInsanePost.js
@@ -1,6 +1,13 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
+import svgicon from 'vue-svgicon';
+import DaLineClamp from '../src/components/DaLineClamp.vue';
 import DaInsanePost from '../src/components/DaInsanePost.vue';
 import posts from '../src/posts';
+
+const localVue = createLocalVue();
+
+localVue.use(svgicon);
+localVue.component('da-line-clamp', DaLineClamp);
 
 it('should emit click event', () => {
   const post = posts[0];
@@ -26,23 +33,27 @@ it('should emit menu event on click', () => {
 it('should set bookmark button title', () => {
   const post = posts[0];
   const wrapper = shallowMount(DaInsanePost, { propsData: { post } });
-  expect(wrapper.find('.insane__reveal__bookmark').element.title).toEqual('Bookmark');
+  expect(wrapper.find('.insane__reveal__bookmark').element.title)
+    .toEqual('Bookmark');
 });
 
 it('should set bookmark button title when bookmarked', () => {
   const post = posts[1];
   const wrapper = shallowMount(DaInsanePost, { propsData: { post } });
-  expect(wrapper.find('.insane__reveal__bookmark').element.title).toEqual('Remove bookmark');
+  expect(wrapper.find('.insane__reveal__bookmark').element.title)
+    .toEqual('Remove bookmark');
 });
 
 it('should set bookmarked class when bookmarked', () => {
   const post = posts[1];
   const wrapper = shallowMount(DaInsanePost, { propsData: { post } });
-  expect(wrapper.find('.insane--post').element.classList.contains('bookmarked')).toEqual(true);
+  expect(wrapper.find('.insane--post').element.classList.contains('bookmarked'))
+    .toEqual(true);
 });
 
 it('should set tags', () => {
   const post = posts[1];
-  const wrapper = shallowMount(DaInsanePost, { propsData: { post } });
-  expect(wrapper.find('.insane__tags').element.innerHTML).toEqual('#javascript,#webdev,#html,#html5');
+  const wrapper = mount(DaInsanePost, { localVue, propsData: { post } });
+  expect(wrapper.find('.insane__tags span').element.innerHTML)
+    .toEqual('#javascript,#webdev,#html,#html5');
 });
