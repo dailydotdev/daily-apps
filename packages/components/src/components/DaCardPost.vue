8120d7c48f51f5d74f9d61e9380a5be394505957
Justin Tan <justin.njl.tan@gmail.com>
2019-08-16 20:26:52 +1200

    add emit event on publication icon click

--- packages/components/src/components/DaCardPost.vue
+++ packages/components/src/components/DaCardPost.vue
@@ -8,7 +8,8 @@
       <img class="card__footer__icon lazyload"
            :data-src="post.publication.image"
            :alt="post.publication.name" :title="post.publication.name"
-           v-if="post.publication.name" :key="post.publication.name"/>
+           v-if="post.publication.name" :key="post.publication.name"
+           @click="$emit('publication', { pub: post.publication })"/>
       <span class="card__footer__views micro2"
             v-if="post.readTime">// {{post.readTime}} min read</span>
       <button class="btn-icon btn-small card__footer__bookmark"
