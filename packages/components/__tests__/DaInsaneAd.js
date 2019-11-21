11c1d2267737219f0a4dd5ac1c05c2669f060899
Ido Shamun <ido@elegantmonkeys.com>
2019-11-21 17:47:31 +0200

    test(components): fix broken tests

--- packages/components/__tests__/DaInsaneAd.js
+++ packages/components/__tests__/DaInsaneAd.js
@@ -24,11 +24,11 @@ it('should set pixel to an empty array when not defined', () => {
 it('should set promoted to default when company is not defined', () => {
   const ad = ads[0];
   const wrapper = shallowMount(DaInsaneAd, { propsData: { ad } });
-  expect(wrapper.vm.promoted).toEqual('Promoted');
+  expect(wrapper.vm.promoted).toEqual('/* Promoted */');
 });
 
 it('should set promoted', () => {
   const ad = ads[1];
   const wrapper = shallowMount(DaInsaneAd, { propsData: { ad } });
-  expect(wrapper.vm.promoted).toEqual('Promoted by CodeFund');
+  expect(wrapper.vm.promoted).toEqual('/* Promoted by CodeFund */');
 });
