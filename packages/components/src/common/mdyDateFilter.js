7698dc4feaa6db927ee911b552c814956410761f
Damiano Magrini <30448081+DamianoMagrini@users.noreply.github.com>
2020-04-29 14:06:20 +0200

    feat(components): update mdyDateFilter

--- packages/components/src/common/mdyDateFilter.js
+++ packages/components/src/common/mdyDateFilter.js
@@ -1,19 +1,74 @@
 // Month abbreviation, Day with leading zeros, Year Filter (Jun 30, 2019)
 
+// Time spans in milliseconds, used for comparing dates. Month and year variable, thus omitted.
+const oneMinute = 60000;
+const oneHour = 3600000;
+const oneDay = 86400000;
+const oneWeek = 604800000;
+const fourWeeks = 2419200000;
+
+/**
+ * Returns the number of milliseconds of a given date since the start of the year.
+ * @param {Date} date
+ * @returns {number}
+ */
+const getYearMs = function getYearMilliseconds(date) {
+  return date - new Date(date.getFullYear(), 0, 0);
+};
+
+/**
+ * Returns whether a given date is yesterday.
+ * @param {Date} date
+ * @returns {Boolean}
+ */
+const isYesterday = function dateIsYesterday(date, now = new Date()) {
+  const dateMs = date.getTime();
+  const dateDayMs = dateMs - (dateMs % oneDay);
+
+  const nowMs = now.getTime();
+  const nowDayMs = nowMs - (nowMs % oneDay);
+
+  return dateDayMs === nowDayMs - oneDay;
+};
+
 export default (value, now = new Date()) => {
   const date = new Date(value);
-  let dt = (now - date) / (1000 * 60);
-  if (dt < 1) {
-    return 'now';
+
+  // Calculate time delta in milliseconds.
+  const dt = now - date;
+
+  if (dt <= oneMinute) return 'now';
+
+  if (dt <= oneHour) {
+    const numMinutes = Math.round(dt / oneMinute);
+    return `${numMinutes} ${numMinutes === 1 ? 'minute' : 'minutes'} ago`;
   }
 
-  if (dt < 59) {
-    return `${Math.round(dt)}m ago`;
+  if (dt <= oneDay) {
+    const numHours = Math.round(dt / oneHour);
+    return `${numHours} ${numHours === 1 ? 'hour' : 'hours'} ago`;
   }
 
-  dt /= 60;
-  if (dt < 48) {
-    return `${Math.round(dt)}h ago`;
+  if (isYesterday(date, now)) return 'yesterday';
+
+  if (dt <= oneWeek) {
+    const numDays = Math.round(dt / oneDay);
+    return `${numDays} ${numDays === 1 ? 'day' : 'days'} ago`;
+  }
+
+  if (
+    dt <= fourWeeks
+    || (date.getFullYear() === now.getFullYear() && date.getMonth() === now.getMonth())
+  ) {
+    const numWeeks = Math.round(dt / oneWeek);
+    return `${numWeeks} ${numWeeks === 1 ? 'week' : 'weeks'} ago`;
+  }
+
+  if (date.getFullYear() === now.getFullYear()) {
+    return `this ${date.toLocaleString('en-US', { month: 'short' })}`;
+  }
+  if (date.getFullYear() === now.getFullYear() - 1 && getYearMs(date) >= getYearMs(now)) {
+    return `last ${date.toLocaleString('en-US', { month: 'short' })}`;
   }
 
   return date.toLocaleString('en-US', {
