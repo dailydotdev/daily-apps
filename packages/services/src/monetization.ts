97e6fdc1fd55ce524462d49af855c5993f8658fc
Ido Shamun <idoesh1@gmail.com>
2020-06-27 19:58:07 +0300

    refactor: replace axios with redaxios to reduce bundle size

--- packages/services/src/monetization.ts
+++ packages/services/src/monetization.ts
@@ -1,37 +1,39 @@
-import axios, {AxiosInstance} from 'axios';
+// @ts-ignore
+import axios, {AxiosInstance} from 'redaxios';
 import {ratioToSize} from './utils';
 
 export interface Ad {
-    link: string;
-    description: string;
-    image: string;
-    size: string;
-    placeholder?: string;
-    company?: string;
-    pixel?: string[];
-    backgroundColor?: string;
-    source: string;
-    referralLink?: string;
+  link: string;
+  description: string;
+  image: string;
+  size: string;
+  placeholder?: string;
+  company?: string;
+  pixel?: string[];
+  backgroundColor?: string;
+  source: string;
+  referralLink?: string;
 }
 
 export interface MonetizationService {
-    fetchAd(): Promise<Ad[]>;
+  fetchAd(): Promise<Ad[]>;
 }
 
 export class MonetizationServiceImpl implements MonetizationService {
-    private readonly request: AxiosInstance;
+  private readonly request: AxiosInstance;
+  private readonly baseURL: string;
 
-    constructor(baseURL: string, app: string | null = null) {
-        this.request = axios.create({
-            baseURL,
-            withCredentials: true,
-            timeout: 10000,
-            headers: app ? {app} : {},
-        });
-    }
+  constructor(baseURL: string, app: string | null = null) {
+    this.baseURL = baseURL;
+    this.request = axios.create({
+      withCredentials: true,
+      timeout: 10000,
+      headers: app ? {app} : {},
+    });
+  }
 
-    async fetchAd(): Promise<Ad[]> {
-        const res = await this.request.get<any[]>('/v1/a');
-        return res.data.map(x => ratioToSize(x));
-    }
+  async fetchAd(): Promise<Ad[]> {
+    const res = await this.request.get(`${this.baseURL}/v1/a`);
+    return JSON.parse(res.data).map((x: any) => ratioToSize(x));
+  }
 }
--- packages/services/src/theme.ts
+++ packages/services/src/theme.ts
@@ -3,24 +3,24 @@ export const themes: string[] = ['darcula', 'bright'];
 export const defaultTheme: string = themes[0];
 
 export function applyTheme(document: Document, newTheme: string, currentTheme: string | null): void {
-    if (currentTheme === newTheme || !document.documentElement) {
-        return;
-    }
+  if (currentTheme === newTheme || !document.documentElement) {
+    return;
+  }
 
-    if (currentTheme) {
-        document.documentElement.classList.add('theming');
-        document.documentElement.addEventListener('transitionend', () => {
-            if (document.documentElement) {
-                document.documentElement.classList.remove('theming');
-            }
-        }, {once: true});
+  if (currentTheme) {
+    document.documentElement.classList.add('theming');
+    document.documentElement.addEventListener('transitionend', () => {
+      if (document.documentElement) {
+        document.documentElement.classList.remove('theming');
+      }
+    }, {once: true});
 
-        if (currentTheme !== defaultTheme) {
-            document.documentElement.classList.remove(currentTheme);
-        }
+    if (currentTheme !== defaultTheme) {
+      document.documentElement.classList.remove(currentTheme);
     }
+  }
 
-    if (newTheme !== defaultTheme) {
-        document.documentElement.classList.add(newTheme);
-    }
+  if (newTheme !== defaultTheme) {
+    document.documentElement.classList.add(newTheme);
+  }
 }
