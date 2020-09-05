8d7d322858dd652d834bd58ecfc64928ad875dab
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-05 19:25:58 +0400

    documentation: domHelper.js

--- packages/components/src/common/domHelper.js
+++ packages/components/src/common/domHelper.js
@@ -1,9 +1,16 @@
+/**
+ * Returns an `HTMLElement` of the `feed` Component 
+ * @param  {boolean} [insaneMode=false] - `state` property for display mode
+ */
 export function getFeedElement(insaneMode = false) {
   const feedClass = insaneMode ? 'feed__insane' : 'feed__cards';
 
   return document.getElementsByClassName(feedClass)[0];
 }
 
+/**
+ * Returns the value for `cards` per row to identify the jump in `index` when navigatong `above` or `below` post
+ */
 export function getCardsPerRow() {
   const feed = getFeedElement();
 
@@ -25,6 +32,15 @@ export function getCardsPerRow() {
   return 0;
 }
 
+/**
+ * Returns the `previousElementSibling` of the currently `hoveredPost` `element`.
+ * <pre> If none - return the currently selected values
+ * <pre> If `insaneMode` is true - immediately return the `previousElementSibling`</pre>
+ * <pre> If `offsetTop` of the `previousElementSibling` is not equal - it indicates they're not on the same row - leftmost post is the currently selected one</pre>
+ * @param  {HTMLElement} el - the currently selected `post` Component's element
+ * @param  {number} index - the current index of the `el` parameter from its siblings
+ * @param  {boolean} [insaneMode=false] - `state` property for display mode
+ */
 export function getPreviousPost(el, index, insaneMode = false) {
   if (!el.previousElementSibling) return [el, index];
 
@@ -35,6 +51,15 @@ export function getPreviousPost(el, index, insaneMode = false) {
   return [el.previousElementSibling, index - 1];
 }
 
+/**
+ * Returns the `nextElementSibling` of the currently `hoveredPost` `element`.
+ * <pre> If none - return the currently selected values
+ * <pre> If `insaneMode` is true - immediately return the `nextElementSibling`</pre>
+ * <pre> If `offsetTop` of the `nextElementSibling` is not equal - it indicates they're not on the same row - rightmost post is the currently selected one</pre>
+ * @param  {HTMLElement} el - the currently selected `post` Component's element
+ * @param  {number} index - the current index of the `el` parameter from its siblings
+ * @param  {boolean} [insaneMode=false] - `state` property for display mode
+ */
 export function getNextPost(el, index, insaneMode = false) {
   if (!el.nextElementSibling) return [el, index];
 
@@ -45,7 +70,14 @@ export function getNextPost(el, index, insaneMode = false) {
   return [el.nextElementSibling, index + 1];
 }
 
-export function getBelowPost(el, index, insaneMode) {
+/**
+ * Returns the `post` `element` directly below the currently selected one.
+ * <pre>If the sum of current index and cards per row exceeds the length, this indicates there's no item beneath</pre>
+ * @param  {HTMLElement} el - the currently selected `post` Component's element
+ * @param  {number} index - the current index of the `el` parameter from its siblings
+ * @param  {boolean} [insaneMode=false] - `state` property for display mode
+ */
+export function getBelowPost(el, index, insaneMode = false) {
   if (insaneMode) return getNextPost(el, index, insaneMode);
 
   const cardsPerRow = getCardsPerRow();
@@ -56,7 +88,14 @@ export function getBelowPost(el, index, insaneMode) {
   return [feed.childNodes[index + cardsPerRow], index + cardsPerRow];
 }
 
-export function getAbovePost(el, index, insaneMode) {
+/**
+ * Returns the `post` `element` directly above the currently selected one.
+ * <pre>If the difference of current index and cards per row falls below 0, this indicates there's no item below as array indexes start at 0</pre>
+ * @param  {HTMLElement} el - the currently selected `post` Component's element
+ * @param  {number} index - the current index of the `el` parameter from its siblings
+ * @param  {boolean} [insaneMode=false] - `state` property for display mode
+ */
+export function getAbovePost(el, index, insaneMode = false) {
   if (insaneMode) return getPreviousPost(el, index, insaneMode);
 
   const cardsPerRow = getCardsPerRow();
@@ -67,7 +106,11 @@ export function getAbovePost(el, index, insaneMode) {
   return [feed.childNodes[index - cardsPerRow], index - cardsPerRow];
 }
 
-export function getFirstPostOnFeed(insaneMode) {
+/**
+ * Returns the `firstElementChild` of the `feed` Component
+ * @param  {boolean} [insaneMode=false] - `state` property for display mode
+ */
+export function getFirstPostOnFeed(insaneMode = false) {
   const feed = getFeedElement(insaneMode);
 
   if (!feed) return null;
